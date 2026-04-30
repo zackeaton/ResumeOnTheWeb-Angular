418af2e7d769805df38bc958eb754ec5cfc0a369
Zack <zackaryeaton@gmail.com>
2026-04-30 12:02:42 -0400

    Updated Skills and Work

--- src/app/sections/right/work-experience/work-experience.component.ts
+++ src/app/sections/right/work-experience/work-experience.component.ts
@@ -12,29 +12,22 @@ export class WorkExperienceComponent implements OnInit {
       logo: '/assets/work/brooklynbrewery.png',
       title: 'Brooklyn Brewery',
       position: 'Digital Marketing Manager',
-      specialization: '',
+      specialization: 'Shopify, Klaviyo, Project Management',
       period: 'Oct \'24 - current',
     },
     {
       logo: '/assets/work/sikich.png',
       title: 'Sikich LLC.',
       position: 'Full-Stack Web Developer & Digital Marketing Specialist',
-      specialization: 'Wordpress, PHP, AJAX, Javascript',
+      specialization: 'WordPress, Shopify, Project Management',
       period: 'April \'21 - Sept \'24',
     },
     {
       logo: '/assets/work/zackeaton.png',
       title: 'Freelance',
       position: 'Web Developer',
-      specialization: 'Wordpress, Web Hosting, Javascript',
+      specialization: 'Shopify, Web Hosting, Project Managment',
       period: 'April \'20 - April \'21',
-    },
-    {
-      logo: '/assets/work/dcds.png',
-      title: 'DC Dev Shop',
-      position: 'Business Development Manager',
-      specialization: 'Sales, Marketing, HubSpot, Client Relations',
-      period: 'Aug \'20 - Oct \'20',
     },
     {
       logo: '/assets/work/eyepromise.png',
@@ -42,20 +35,6 @@ export class WorkExperienceComponent implements OnInit {
       position: 'Esports Product Manager',
       specialization: 'Social Media, Influencer Marketing, Content Creation',
       period: 'Jan \'19 - April \'20',
-    },
-    {
-      logo: '/assets/work/eaton.png',
-      title: 'Eaton Properties',
-      position: 'Real Estate Agent',
-      specialization: 'Property Management, Real Estate Sales',
-      period: 'Aug \'15 - May \'19',
-    },
-    {
-      logo: '/assets/work/strange.png',
-      title: 'Strange Donuts',
-      position: 'Front of House',
-      specialization: 'Sales, Customer Service, Delivery',
-      period: 'July \'15 - Sept \'16',
     }
   ];
 
